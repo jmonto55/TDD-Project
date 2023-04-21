8c49a4401e110a111866beb1b3a0e833a0fd8ebc
jmonto55 <jmonto55@gmail.com>
2023-04-21 17:05:21 -0500

    Build test for the reverse method

--- lib/solver.rb
+++ lib/solver.rb
@@ -3,6 +3,8 @@ class Solver
     raise ArgumentError, 'Factorial is undefined for negative integers' unless num.is_a?(Integer) && num >= 0
 
     return 1 if num.zero?
+
+    (1..num).reduce(:*)
   end
 
 end
