5fb790d2664e0963a321b4933e7387c3bd7c798a
jmonto55 <jmonto55@gmail.com>
2023-04-21 17:02:55 -0500

    Build tests to check if the number passed to factorial is int >= 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,5 +6,8 @@ describe Solver do
     it 'Should test if factorial of 0 is 1' do
       expect(solver.factorial(0)).to eq 1
     end
+    it 'Should test if error rises when num is not integer or less than 0' do
+      expect { solver.factorial(-10) }.to raise_error(ArgumentError, 'Factorial is undefined for negative integers')
+    end
   end
 end
