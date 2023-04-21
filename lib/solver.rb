0bc8daf0afb606a14148dafb12cbb34fd46b68f0
jmonto55 <jmonto55@gmail.com>
2023-04-21 17:04:00 -0500

    Add condition to raise an error when actorial method gets a not integer or negative argument

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,5 +1,7 @@
 class Solver
   def factorial(num)
+    raise ArgumentError, 'Factorial is undefined for negative integers' unless num.is_a?(Integer) && num >= 0
+
     return 1 if num.zero?
   end
 
