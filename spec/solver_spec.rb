aae239f154cc86ffdc1add8e2286a2e40cdcac0b
jmonto55 <jmonto55@gmail.com>
2023-04-21 17:37:50 -0500

    Fix rubocop errors

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,10 +6,10 @@ describe Solver do
     it 'Should test if factorial of 0 is 1' do
       expect(solver.factorial(0)).to eq 1
     end
-    it '' do
+    it 'Should test if error rises when num is not integer or less than 0' do
       expect { solver.factorial(-10) }.to raise_error(ArgumentError, 'Factorial is undefined for negative integers')
     end
-    it '' do
+    it 'Should test if factorial is working' do
       expect(solver.factorial(5)).to eq 120
     end
   end
