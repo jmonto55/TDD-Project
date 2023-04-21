313800089a60fa44ad1397dddb1d8b0ca32fd5dd
jmonto55 <jmonto55@gmail.com>
2023-04-21 14:56:15 -0500

    Build tests to check if the number passed to factorial is int >= 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,10 +2,14 @@ require_relative '../lib/solver'
 
 describe Solver do
   context 'Should create a new Solver class and tests its methods' do
+    solver = Solver.new
     it 'Should test if factorial of 0 is 1' do
-      solver = Solver.new
 
       expect(solver.factorial(0)).to eq 1
+    end
+    it '' do
+
+      expect { solve.factorial('jose') }.to raise_error('error')
     end
   end
 end
