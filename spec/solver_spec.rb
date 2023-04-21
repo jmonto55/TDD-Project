38d5659e023e60130e6dcb1c679790d4910c98a3
jmonto55 <jmonto55@gmail.com>
2023-04-21 16:59:58 -0500

    Build test for factorial of 0 special case

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/solver'
 
 describe Solver do
-
+  context 'Should create a new Solver class and test factorial method' do
+    solver = Solver.new
+    it 'Should test if factorial of 0 is 1' do
+      expect(solver.factorial(0)).to eq 1
+    end
+  end
 end
