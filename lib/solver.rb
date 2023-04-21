2826e2efc8f3b2a2e4f917ce17865094800006e5
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-04-21 23:57:47 +0300

    Build a Class solver with fizzbuzz method returning fizzbuzz if num is divisible by 3 and  5

--- lib/solver.rb
+++ lib/solver.rb
@@ -14,8 +14,11 @@ class Solver
   end
 
   def fizzbuzz(num)
+    return "fizzbuzz" if ((num % 3).zero? && (num % 5).zero?)
     return "fizz" if (num % 3).zero?
     
-    return "buzz" if (num % 5).zero?
+    "buzz" if (num % 5).zero?
+
+    
   end
 end
